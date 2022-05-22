726bcaa0e9568dcaa70ef89666a166cf291b044d
Jeppe Cramon <jeppe@cloudcreate.dk>
2022-05-22 21:15:18 +0200

    Added support Nested UnitOfWorks when using `UnitOfWorkFactory#withUnitOfWork` and `UnitOfWorkFactory#usingUnitOfWork`

--- common-types/src/main/java/dk/cloudcreate/essentials/components/common/transaction/UnitOfWorkFactory.java
+++ common-types/src/main/java/dk/cloudcreate/essentials/components/common/transaction/UnitOfWorkFactory.java
@@ -1,6 +1,7 @@
 package dk.cloudcreate.essentials.components.common.transaction;
 
 import dk.cloudcreate.essentials.shared.functional.*;
+import org.slf4j.*;
 
 import java.util.Optional;
 
@@ -11,6 +12,8 @@ import static dk.cloudcreate.essentials.shared.FailFast.requireNonNull;
  * @param <UOW> the {@link UnitOfWork} sub-type returned by the {@link UnitOfWorkFactory}
  */
 public interface UnitOfWorkFactory<UOW extends UnitOfWork> {
+    Logger unitOfWorkLog = LoggerFactory.getLogger(UnitOfWorkFactory.class);
+
     /**
      * Get a required active {@link UnitOfWork}
      *
@@ -30,25 +33,59 @@ public interface UnitOfWorkFactory<UOW extends UnitOfWork> {
 
     default void usingUnitOfWork(CheckedConsumer<UOW> unitOfWorkConsumer) {
         requireNonNull(unitOfWorkConsumer, "No unitOfWorkConsumer provided");
-        var unitOfWork = getOrCreateNewUnitOfWork();
+
+        var existingUnitOfWork = getCurrentUnitOfWork();
+        var unitOfWork = existingUnitOfWork.orElseGet(() -> {
+            unitOfWorkLog.debug("Creating a new UnitOfWork for this usingUnitOfWork(CheckedConsumer) method call as there wasn't an existing UnitOfWork");
+            return getOrCreateNewUnitOfWork();
+        });
+        existingUnitOfWork.ifPresent(uow -> unitOfWorkLog.debug("NestedUnitOfWork: Reusing existing UnitOfWork for this usingUnitOfWork(CheckedConsumer) method call"));
         try {
             unitOfWorkConsumer.accept(unitOfWork);
-            unitOfWork.commit();
+            if (existingUnitOfWork.isEmpty()) {
+                unitOfWorkLog.debug("Committing the UnitOfWork created by this usingUnitOfWork(CheckedConsumer) method call");
+                unitOfWork.commit();
+            } else {
+                unitOfWorkLog.debug("NestedUnitOfWork: Won't commit the UnitOfWork as it wasn't created by this usingUnitOfWork(CheckedConsumer) method call");
+            }
         } catch (Exception e) {
-            unitOfWork.rollback(e);
+            if (existingUnitOfWork.isEmpty()) {
+                unitOfWorkLog.debug("Rolling back the UnitOfWork created by this usingUnitOfWork(CheckedConsumer) method call");
+                unitOfWork.rollback(e);
+            } else {
+                unitOfWorkLog.debug("NestedUnitOfWork: Marking UnitOfWork as rollback only as it wasn't created by this usingUnitOfWork(CheckedConsumer) method call");
+                unitOfWork.markAsRollbackOnly(e);
+            }
             throw new UnitOfWorkException(e);
         }
     }
 
     default <R> R withUnitOfWork(CheckedFunction<UOW, R> unitOfWorkFunction) {
         requireNonNull(unitOfWorkFunction, "No unitOfWorkFunction provided");
-        var unitOfWork = getOrCreateNewUnitOfWork();
+        var existingUnitOfWork = getCurrentUnitOfWork();
+        var unitOfWork = existingUnitOfWork.orElseGet(() -> {
+            unitOfWorkLog.debug("Creating a new UnitOfWork for this withUnitOfWork(CheckedFunction) method call as there wasn't an existing UnitOfWork");
+            return getOrCreateNewUnitOfWork();
+        });
+        existingUnitOfWork.ifPresent(uow -> unitOfWorkLog.debug("NestedUnitOfWork: Reusing existing UnitOfWork for this withUnitOfWork(CheckedFunction) method call"));
         try {
             var result = unitOfWorkFunction.apply(unitOfWork);
-            unitOfWork.commit();
+            if (existingUnitOfWork.isEmpty()) {
+                unitOfWorkLog.debug("Committing the UnitOfWork created by this withUnitOfWork(CheckedFunction) method call");
+                unitOfWork.commit();
+            } else {
+                unitOfWorkLog.debug("NestedUnitOfWork: Won't commit the UnitOfWork as it wasn't created by this withUnitOfWork(CheckedFunction) method call");
+            }
+
             return result;
         } catch (Exception e) {
-            unitOfWork.rollback(e);
+            if (existingUnitOfWork.isEmpty()) {
+                unitOfWorkLog.debug("Rolling back the UnitOfWork created by this withUnitOfWork(CheckedFunction) method call");
+                unitOfWork.rollback(e);
+            } else {
+                unitOfWorkLog.debug("NestedUnitOfWork: Marking UnitOfWork as rollback only as it wasn't created by this withUnitOfWork(CheckedFunction) method call");
+                unitOfWork.markAsRollbackOnly(e);
+            }
             throw new UnitOfWorkException(e);
         }
     }
